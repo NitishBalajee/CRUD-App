b4b01c545423741289cffb14a3eb57942f6cc36c
lakshmi <kirthilaku98@gmail.com>
2020-08-14 18:17:08 +0530

    change

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,8 @@ var mongoose=require('mongoose');
 var config=require('./config/databaseconfig');
 mongoose.Promise= global.Promise;
 mongoose.connect(config.url).then(() => {
+  
+  console.log("test3");
   console.log("connect successfully");
   console.log("test2");
 });
