96092d9586f5d0bb2baab59e84b8c4e11e4c151c
NITISH BALAJEE SHANMUGAM <SESA527945@se.com>
2020-08-14 17:58:46 +0530

    changed dev

--- backend/database.js
+++ backend/database.js
@@ -3,4 +3,5 @@ var config=require('./config/databaseconfig');
 mongoose.Promise= global.Promise;
 mongoose.connect(config.url).then(() => {
   console.log("connected successfully");
+  console.log("test");
 });
