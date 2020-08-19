3f861013a6b265ee88963e573a78b1be406c8be4
lakshmi <kirthilaku98@gmail.com>
2020-08-19 18:43:39 +0530

    Signed-off-by: lakshmi <kirthilaku98@gmail.com>

--- backend/database.js
+++ backend/database.js
@@ -3,8 +3,6 @@ var config=require('./config/databaseconfig');
 mongoose.Promise= global.Promise;
 mongoose.connect(config.url).then(() => {
 
-  console.log("test");
   console.log("connect successfully");
-  console.log("test2");
 });
 
