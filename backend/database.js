135661fa8b3c0f99401223acaf9147e2afc7fa4b
lakshmi <kirthilaku98@gmail.com>
2020-08-19 19:11:30 +0530

    revertio

--- backend/database.js
+++ backend/database.js
@@ -3,8 +3,7 @@ var config=require('./config/databaseconfig');
 mongoose.Promise= global.Promise;
 mongoose.connect(config.url).then(() => {
 
-console.log();
-	console.log("test");
+  console.log("test");
   console.log("connect successfully");
   console.log("test2");
 });
